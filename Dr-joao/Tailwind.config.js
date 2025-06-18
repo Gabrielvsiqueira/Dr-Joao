5ddbaeb2434d631f83f51ca0fd11f0881c483ab3
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-06-18 20:36:48 -0300

    feat: structure to tailwind config

--- Dr-joao/Tailwind.config.js
+++ Dr-joao/Tailwind.config.js
@@ -1,12 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "/index.html", // Se seu HTML principal está na raiz
-    "./src/**/*.{js,ts,jsx,tsx,html}", // Se você tem arquivos em src/
-    // Adicione mais caminhos se seus arquivos HTML/JS/outros estiverem em outras pastas
+    "./index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: { 
+        'golden': '#FFFBDE',
+      },
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+        playfair: ['Playfair Display', 'serif'], 
+      },
+    },
   },
   plugins: [],
 }
