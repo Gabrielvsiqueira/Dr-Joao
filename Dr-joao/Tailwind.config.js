d8e4eb089875688143d30b851133443f9bcc9c43
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-06-26 12:03:06 -0300

    feat: alterações no arquivo de tailwind

--- Dr-joao/Tailwind.config.js
+++ Dr-joao/Tailwind.config.js
@@ -1,6 +1,8 @@
+// tailwind.config.js
 export default {
   content: [
     "./index.html",
+    "./src/**/*.html", // Adiciona todos os arquivos HTML dentro da pasta src e subpastas
   ],
   theme: {
     extend: {
